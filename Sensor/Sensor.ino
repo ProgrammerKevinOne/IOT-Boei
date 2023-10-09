e496c6c15adea2ced58ba5d06b5431585bfbe11c
Rick van der Sommen <rickvdsommen@hotmail.com>
2023-10-09 10:50:01 +0200

    30 sec

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -27,7 +27,7 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 1800;
+const unsigned TX_INTERVAL = 30;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
