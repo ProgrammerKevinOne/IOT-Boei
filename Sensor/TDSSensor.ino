c9907785dd54ae8a91cf3eaafebf7171a0d1451e
Rick van der Sommen <rickvdsommen@hotmail.com>
2023-10-11 10:56:05 +0200

    fix

--- Sensor/TDSSensor.ino
+++ Sensor/TDSSensor.ino
@@ -15,64 +15,57 @@
  ****************************************************/
 
 #define TdsSensorPin A0
-#define VREF 5.0      // analog reference voltage(Volt) of the ADC
-#define SCOUNT  30           // sum of sample point
-int analogBuffer[SCOUNT];    // store the analog value in the array, read from ADC
+#define VREF 5.0           // analog reference voltage(Volt) of the ADC
+#define SCOUNT 30          // sum of sample point
+int analogBuffer[SCOUNT];  // store the analog value in the array, read from ADC
 int analogBufferTemp[SCOUNT];
-int analogBufferIndex = 0,copyIndex = 0;
-float averageVoltage = 0,tdsValue = 0,temperature = 25;
+int analogBufferIndex = 0, copyIndex = 0;
+float averageVoltage = 0, tdsValue = 0, temperature = 25;
 
-void initializeTDSSensor(){
-
-    
-    pinMode(TdsSensorPin,INPUT);
-
+void initializeTDSSensor() {
+  pinMode(TdsSensorPin, INPUT);
 }
 
-int getMedianNum(int bArray[], int iFilterLen) 
-{
-      int bTab[iFilterLen];
-      for (byte i = 0; i<iFilterLen; i++)
-      bTab[i] = bArray[i];
-      int i, j, bTemp;
-      for (j = 0; j < iFilterLen - 1; j++) 
-      {
-      for (i = 0; i < iFilterLen - j - 1; i++) 
-          {
-        if (bTab[i] > bTab[i + 1]) 
-            {
+int getMedianNum(int bArray[], int iFilterLen) {
+  int bTab[iFilterLen];
+  for (byte i = 0; i < iFilterLen; i++)
+    bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
         bTemp = bTab[i];
-            bTab[i] = bTab[i + 1];
+        bTab[i] = bTab[i + 1];
         bTab[i + 1] = bTemp;
-         }
       }
-      }
-      if ((iFilterLen & 1) > 0)
+    }
+  }
+  if ((iFilterLen & 1) > 0)
     bTemp = bTab[(iFilterLen - 1) / 2];
-      else
+  else
     bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-      return bTemp;
+  return bTemp;
 }
 
-float measureTDS(){
+float measureTDS() {
 
-static unsigned long analogSampleTimepoint = millis();
-  for(int i=0;i<SCOUNT;i++){
+  static unsigned long analogSampleTimepoint = millis();
+  for (int i = 0; i < SCOUNT; i++) {
     delay(40);
-    analogBuffer[i] = analogRead(TdsSensorPin);//fill analogBuffer with 30 measurement values. there is 40 ms between every measurement.
+    analogBuffer[i] = analogRead(TdsSensorPin);  //fill analogBuffer with 30 measurement values. there is 40 ms between every measurement.
   }
 
 
-      
-      
-  averageVoltage = getMedianNum(analogBuffer,SCOUNT) * (float)VREF / 1024.0; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
-  float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
-  float compensationVoltage=averageVoltage/compensationCoefficient;  //temperature compensation
-  tdsValue=(133.42*compensationVoltage*compensationVoltage*compensationVoltage - 255.86*compensationVoltage*compensationVoltage + 857.39*compensationVoltage)*0.5; //convert voltage value to tds value
-      
+
+
+  averageVoltage = getMedianNum(analogBuffer, SCOUNT) * (float)VREF / 1024.0;                                                                                                       // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+  float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);                                                                                                                //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+  float compensationVoltage = averageVoltage / compensationCoefficient;                                                                                                             //temperature compensation
+  tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;  //convert voltage value to tds value
+
   Serial.print("TDS Value: ");
-  Serial.print(tdsValue,0);
+  Serial.print(tdsValue, 0);
   Serial.println(" ppm");
-      
+
   return tdsValue;
 }
--- Sensor/TemperatureSensor.ino
+++ Sensor/TemperatureSensor.ino
@@ -1,27 +1,24 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
- 
+
 // Connect your yellow pin to Pin12 on Arduino
 #define ONE_WIRE_BUS 5
- 
+
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
- 
-// Pass our oneWire reference to Dallas Temperature sensor 
+
+// Pass our oneWire reference to Dallas Temperature sensor
 DallasTemperature sensors(&oneWire);
- 
-void initializeTemperatureSensor()
-{
+
+void initializeTemperatureSensor() {
   // initialize the Serial Monitor at a baud rate of 9600.
-  
   sensors.begin();
 }
- 
-float measureTemperature(){ 
+
+float measureTemperature() {
   // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
   sensors.requestTemperatures();
   Serial.print("Celsius temperature: ");
   Serial.println(sensors.getTempCByIndex(0));
   return sensors.getTempCByIndex(0);
-  
 }
