1043d1bc5d42056601e7f4904878d3d92c2eddc3
Kevin <kevinkoolen@outlook.com>
2024-01-21 22:39:09 +0100

    email

--- src/Components/profile.js
+++ src/Components/profile.js
@@ -11,8 +11,6 @@ const Profile = () => {
   return (
     isAuthenticated && (
       <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
         <p>{user.email}</p>
       </div>
     )
